a559f33dff43f5aa69233f165279f9fb525672f4
Murali <murali.thyagarajan87@gmail.com>
2019-09-10 15:46:12 -0700

    Create sentence shuffle algorithm
    Based on Knuth's shuffle. Shuffles the words in a sentence in O(n) time. There is no check for the following, 
    - Delimiters other than empty space
    - Length of input string and output string will vary if the string starts with blankspace and trailing characters

---
+++ MyProblems/sentenceShuffle.java
@@ -0,0 +1,59 @@
+/*
+ * Given a string, randomize the words in the string and return the output
+ * 
+ * Given input string is bound by following conditions,
+ * ****** Only delimiter is a blankspace character ******
+ * ****** Extent of randomization needs to equal to the probability of getting the same output ******
+ * 
+ * Example 
+ * 
+ * Input String - "Life is good"
+ * Output String - "good is Life" 
+ * 
+ * */
+import java.util.*;
+public class shuffleString {	
+	public static void main(String[] args){
+		String str = "   Life is good   ";
+		String output = randomizeStr(str);
+		System.out.println("Randomized String is : " + output);
+		System.out.print("Length of input is : " + str.length() + " Length of output is : " + output.length());
+	}
+	
+	public static String randomizeStr(String str){
+		
+		if(str == null || str.length() == 0){return str;}
+		
+		final String[] splitInput = str.trim().split("\\s+");
+		int len = splitInput.length;
+		System.out.println("The length is : " + len);
+		if(len < 2 ) {return str;}
+		StringBuilder sb = new StringBuilder();
+		
+		Random rand = new Random();
+				
+		for(int i = len - 1; i > 0; i--) {
+			int randIdx = rand.nextInt(i);
+			swapRandom(splitInput, randIdx, i);			
+		}
+		
+		for(int j = 0; j < splitInput.length; j++) {
+			if(j == splitInput.length - 1) {
+				sb.append(splitInput[j]);
+			}else {
+				sb.append(splitInput[j]);
+				sb.append(" ");
+			}
+		}
+		
+		return sb.toString();
+	}
+	
+	public static void swapRandom(String[] input, int idx1, int idx2) {
+		String temp = input[idx1];
+		input[idx1] = input[idx2];
+		input[idx2] = temp;		
+	}
+	
+	
+}
